ed1657ba40fdd0ca28d2be28b51e73ad97b41c25
sahil <sahil91098singh@gmail.com>
2019-07-20 15:10:13 +0530

    Files committed.

---
+++ app/src/main/java/com/example/dynamicbackground/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.dynamicbackground;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.relative);
+        AnimationDrawable animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+    }
+}
